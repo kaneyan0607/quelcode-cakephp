20401b1898bba10114ca662dcf923db423032a80
kaneyan0607 <kaneyan0607@gmail.com>
2020-12-05 02:25:59 +0900

    add:静的ファイルとコントローラーアクション

---
+++ html/mycakeapp/src/Controller/PostsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+class PostsController extends AppController
+{
+    public $autoRender = false;
+
+    public function index()
+    {
+        echo "Posts Index";
+    }
+
+    public function view($id = null)
+    {
+        echo "ID:" . $id . "Posts View";
+    }
+}
